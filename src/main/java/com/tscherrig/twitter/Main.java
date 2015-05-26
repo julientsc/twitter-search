d9da50ee7824fd3d7dcaecb058d0c1a2f9cbe683
Julien Tscherrig <julien.tscherrig@hefr.ch>
2015-05-26 02:18:29 +0200

    Update

--- src/main/java/com/tscherrig/twitter/Main.java
+++ src/main/java/com/tscherrig/twitter/Main.java
@@ -11,9 +11,7 @@ public class Main {
 
     public static void main(String[] args) {
         ConfigurationBuilder cb = new ConfigurationBuilder();
-        cb.setDebugEnabled(true)
-                .setOAuthConsumerKey("PBwpBVcMe2a5EU2SxZZbUTYC5")
-                .setOAuthConsumerSecret("mhdJ0EhJzWfSxqwSQPRFnijiocvQUgJ2Bo9vB2m6V5toragjBE");
+        Config.setConfig(cb);
         TwitterFactory tf = new TwitterFactory(cb.build());
         Twitter twitter = tf.getInstance();
 
